64a140e74dd29a707ae60793f713871f5eb17fc0
LoxyMono <aidan@fernow.com>
2026-01-05 17:27:40 -0600

    Changed output to print memory maps

--- Bootloaders/BIOS/Source/C/Main.c
+++ Bootloaders/BIOS/Source/C/Main.c
@@ -5,6 +5,8 @@
 #include <IO/Disk.h>
 #include <IO/Ports.h>
 
+#include <Memory/MemoryMap.h>
+
 #include <Utils/String.h>
 
 #include <Video/VGA.h>
@@ -18,4 +20,21 @@ VOID Main()
     VgaResetColor();
     VgaPrintString(" x86 BIOS\r\n");
     VgaPrintString("Compiled on " __DATE__ " at " __TIME__ "\r\n\r\n");
+
+    VgaPrintString("\t\tInitializing loader");
+    
+    DWORD entryCount = 0;
+    MEMORY_MAP_ENTRY *memoryMaps = MemoryMapLoad(&entryCount);
+
+    for (DWORD i = 0; i < entryCount; i++)
+    {
+        MEMORY_MAP_ENTRY *entry = &memoryMaps[i];
+        VgaPrintString("\r\nMemory Map Entry %x: Base=0x%8x_%8x Length=0x%8x_%8x Type=%x",
+            i,
+            entry->baseHigh,
+            entry->baseLow,
+            entry->lengthHigh,
+            entry->lengthLow,
+            entry->type);
+    }
 }
